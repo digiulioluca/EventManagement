0c177bd861a17bc542a3f52d2ed3f2d4ad9f6601
Luca Di Giulio <lkdgl95@gmail.com>
2025-07-13 15:07:31 +0200

    Update RequestDTO with Validations + favicon.ico in /FrontEnd

--- BackEnd/user-service/src/main/java/it/profice/project/user_service/controller/LoginController.java
+++ BackEnd/user-service/src/main/java/it/profice/project/user_service/controller/LoginController.java
@@ -4,6 +4,7 @@ import it.profice.project.user_service.dto.RequestDTO;
 import it.profice.project.user_service.dto.ResponseDTO;
 import it.profice.project.user_service.dto.UserDTO;
 import it.profice.project.user_service.service.LoginService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +17,13 @@ public class LoginController {
     private LoginService loginService;
 
     @GetMapping("/login")
-    public ResponseDTO login(@RequestBody RequestDTO request) {
+    public ResponseDTO login(@RequestBody @Valid RequestDTO request) {
         return loginService.login(request);
     }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO register(@RequestBody UserDTO newUser) {
+    public UserDTO register(@RequestBody @Valid UserDTO newUser) {
         return loginService.register(newUser);
     }
 }
--- BackEnd/user-service/src/main/java/it/profice/project/user_service/dto/RequestDTO.java
+++ BackEnd/user-service/src/main/java/it/profice/project/user_service/dto/RequestDTO.java
@@ -1,5 +1,8 @@
 package it.profice.project.user_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestDTO {
+
+    @Email
+    @NotEmpty
     private String email;
+    @NotBlank
     private String password;
 }
