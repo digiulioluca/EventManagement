b0018d90df253a5a7da4d600099dd84cbb2b19a0
GranCacioRomano <falconetti.grafico@gmail.com>
2025-07-14 17:05:10 +0200

    TEST ReservationServiceImpl GRANCACIOROMANO

--- BackEnd/reservation-service/src/main/java/it/profice/project/reservation_service/service/ReservationServiceImpl.java
+++ BackEnd/reservation-service/src/main/java/it/profice/project/reservation_service/service/ReservationServiceImpl.java
@@ -1,11 +1,14 @@
 package it.profice.project.reservation_service.service;
 
+import it.profice.project.event_service.dto.EventDTO;
+import it.profice.project.reservation_service.config.WebClientBuilderConfig;
 import it.profice.project.reservation_service.dto.ReservationDTO;
 import it.profice.project.reservation_service.model.Reservation;
 import it.profice.project.reservation_service.repository.ReservationRepository;
 import it.profice.project.reservation_service.exception.ReservationNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.UUID;
 public class ReservationServiceImpl implements ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final WebClient.Builder webClientBuilder;
 
     @Override
     public ReservationDTO save(ReservationDTO reservation){
@@ -46,14 +50,27 @@ public class ReservationServiceImpl implements ReservationService {
                 .map(this::modelToDto)
                 .toList();
     }
-    private ReservationDTO modelToDto(Reservation reservation){
+
+    private ReservationDTO modelToDto(Reservation reservation) {
+        EventDTO event = webClientBuilder.build()
+                .get()
+                .uri("http://event-service/api/v1/events/{uuid}", reservation.getEventUuid())
+                .retrieve()
+                .bodyToMono(EventDTO.class)
+                .block();
+
+        if (event == null) {
+            throw new RuntimeException("Evento non trovato per uuid: " + reservation.getEventUuid());
+        }
+
         return ReservationDTO.builder()
-                .uuid(reservation.getUuid())
-                .userUuid(reservation.getUserUuid())
-                .eventUuid(reservation.getEventUuid())
                 .date(reservation.getDate())
+                .eventTitle(event.getTitle())
+                .eventDate(event.getDate())
+                .eventLocation(event.getLocation())
                 .build();
     }
+
 
     private Reservation dtoToModel(ReservationDTO reservation){
         return Reservation.builder()
@@ -63,6 +80,4 @@ public class ReservationServiceImpl implements ReservationService {
                 .date(reservation.getDate())
                 .build();
     }
-
-
 }
