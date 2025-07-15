8cc3fca1fe79a2df580bfc36889991e0aa7e274c
Luca Di Giulio <lkdgl95@gmail.com>
2025-07-15 11:07:34 +0200

    Updated ReservationServiceImpl

--- BackEnd/reservation-service/src/main/java/it/profice/project/reservation_service/service/ReservationServiceImpl.java
+++ BackEnd/reservation-service/src/main/java/it/profice/project/reservation_service/service/ReservationServiceImpl.java
@@ -1,17 +1,18 @@
 package it.profice.project.reservation_service.service;
 
 import it.profice.project.event_service.dto.EventDTO;
-import it.profice.project.reservation_service.config.WebClientBuilderConfig;
 import it.profice.project.reservation_service.dto.ReservationDTO;
 import it.profice.project.reservation_service.model.Reservation;
 import it.profice.project.reservation_service.repository.ReservationRepository;
 import it.profice.project.reservation_service.exception.ReservationNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -62,6 +63,16 @@ public class ReservationServiceImpl implements ReservationService {
         if (event == null) {
             throw new RuntimeException("Evento non trovato per uuid: " + reservation.getEventUuid());
         }
+
+        event = webClientBuilder.build()
+                .patch()
+                .uri("http://event-service/api/v1/events/{uuid}", event.getUuid())
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("availableSeats", event.getAvailableSeats()-1))
+                .retrieve()
+                .bodyToMono(EventDTO.class)
+                .block();
+
 
         return ReservationDTO.builder()
                 .date(reservation.getDate())
