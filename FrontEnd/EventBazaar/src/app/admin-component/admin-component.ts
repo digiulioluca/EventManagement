8db7f88781073e60e378d71515fb64945b246726
GranCacioRomano <falconetti.grafico@gmail.com>
2025-07-16 21:35:15 +0200

    Fix admin page mergiata male

--- FrontEnd/EventBazaar/src/app/admin-component/admin-component.ts
+++ FrontEnd/EventBazaar/src/app/admin-component/admin-component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { UserDTO } from '../module/userDTO';
@@ -11,7 +11,7 @@ import { EventService } from '../service/event.service';
 @Component({
   selector: 'app-admin-component',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin-component.html',
   styleUrl: './admin-component.css'
 })
@@ -19,33 +19,31 @@ export class AdminComponent implements OnInit{
 
   events: EventDTO [] = [];
   event : EventDTO | null = null;
+  editableEvent: EventDTO | null = null;
+  newEvent: EventDTO | null = null;
   isLoading = true;
   editMode = false;
   addMode = false;
   user: UserDTO | null = null;
+  private http = inject(HttpClient);
   eventsState: boolean = true;
   utenteUuid: string | null = null;
   eventCategories = Object.values(EventCategory);
-  addEventForm!: FormGroup;
-  editEventForm!: FormGroup;
-  showValidationErrors = false;
-  private http = inject(HttpClient);
-  todayString: string | null = null;
 
-  constructor(private eventService: EventService, private router: Router, private fb: FormBuilder){}
+
+  constructor(private eventService: EventService, private router: Router){}
 
   ngOnInit(): void {
 
      const uuid = localStorage.getItem('uuid');
      this.utenteUuid = localStorage.getItem('uuid');
-     const today = new Date();
-     this.todayString = today.toISOString().split('T')[0];
+     console.log('UUID dal localStorage:', uuid);
 
      if (!uuid) {
       this.isLoading = false;
       return;
     }
-    
+
       this.http.get<UserDTO>(`http://localhost:8080/api/v1/users/${uuid}`).subscribe({
         next: (res) => {
         this.user = res;
@@ -83,54 +81,49 @@ export class AdminComponent implements OnInit{
   }
 
   enableEdit(): void {
-
     this.editMode = true;
-
     if (this.event) {
-      this.editEventForm = this.fb.group({
-        title: [this.event.title, [Validators.required, Validators.minLength(6), Validators.maxLength(50)]],
-        description: [this.event.description, [Validators.required, Validators.maxLength(100)]],
-        date: [this.event.date, Validators.required],
-        location: [this.event.location, Validators.required],
-        totalSeats: [this.event.totalSeats, [Validators.required, Validators.min(100)]],
-        availableSeats: [this.event.availableSeats],
-        state: [this.event.state],
-        eventCategory: [this.event.eventCategory, Validators.required],
-        userUuid: [this.event.userUuid]
-      });
+      this.editableEvent = {
+        uuid: this.event.uuid,
+        title: this.event.title,
+        description: this.event.description,
+        date: this.event.date,
+        location: this.event.location,
+        totalSeats: this.event.totalSeats,
+        availableSeats: this.event.availableSeats,
+        state: this.event.state,
+        eventCategory: this.event.eventCategory,
+        userUuid: this.event.userUuid
+      };
     }
   }
 
   cancelEdit(): void {
-    this.editMode = false;   
-    this.showValidationErrors = false;
+    this.editMode = false;
+    if (this.event) {
+      this.editableEvent = {
+        uuid: this.event.uuid,
+        title: this.event.title,
+        description: this.event.description,
+        date: this.event.date,
+        location: this.event.location,
+        totalSeats: this.event.totalSeats,
+        availableSeats: this.event.availableSeats,
+        state: this.event.state,
+        eventCategory: this.event.eventCategory,
+        userUuid: this.event.userUuid
+      };
+    }
   }
 
   saveEdit(): void {
-    this.showValidationErrors = true;
+    if (!this.editableEvent || !this.event) return;
 
-    if (!this.editEventForm.valid || !this.event) return;
-
-    const formValue = this.editEventForm.value;
-
-    const editableEvent: EventDTO = {
-        uuid: this.event.uuid,
-        title: formValue.title,
-        description: formValue.description,
-        date: formValue.date,
-        location: formValue.location,
-        totalSeats: formValue.totalSeats,
-        availableSeats: formValue.availableSeats,
-        state: formValue.state,
-        eventCategory: formValue.eventCategory,
-        userUuid: formValue.userUuid
-    }
-
-    this.eventService.update(this.event.uuid, editableEvent).subscribe({
-      next: () => {
+    this.eventService.update(this.event.uuid, this.editableEvent).subscribe({
+      next: (updatedEvent) => {
+        this.event = updatedEvent;
         this.editMode = false;
         this.event = null;
-        this.editEventForm.reset();
         this.showEvent();
       },
       error: (err) => {
@@ -140,18 +133,18 @@ export class AdminComponent implements OnInit{
   }
 
   deleteEvent(): void {
-    this.showValidationErrors = false;
     if (!this.event) return;
 
     const conferma = confirm('Sei sicuro di voler cancellare l\'evento?');
     if (!conferma) return;
 
     this.eventService.delete(this.event.uuid).subscribe({
-      next: () => {
+      next: (res) => {
         alert('Evento eliminato');
         this.editMode = false;
         this.event = null;
         this.showEvent();
+        this.router.navigate(['/admin']);
       },
       error: (err) => {
         console.error('Errore cancellazone', err);
@@ -161,51 +154,33 @@ export class AdminComponent implements OnInit{
 
    addEvent(): void {
     this.addMode = true;
-
-      this.addEventForm = this.fb.group({
-        title: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(50)]],
-        description: ['', [Validators.required, Validators.maxLength(100)]],
-        date: ['', Validators.required],
-        location: ['', Validators.required],
-        totalSeats: [100, [Validators.required, Validators.min(100)]],
-        availableSeats: [0],
-        state: [true],
-        eventCategory: [EventCategory.EMPTY, Validators.required],
-        userUuid: [this.utenteUuid]
-      });
+    if(this.utenteUuid)
+    this.newEvent = {
+      uuid: '',
+      title: '',
+      description: '',
+      date: new Date(),
+      location: '',
+      totalSeats: 100,
+      availableSeats: 0,
+      state: true,
+      eventCategory: EventCategory.EMPTY,
+      userUuid: this.utenteUuid
     };
+  }
 
   cancelAdd(): void {
-    this.showValidationErrors = false;
     this.addMode = false;
   }
 
   saveNewEvent(): void {
-    this.showValidationErrors = true;
-
-    if(this.addEventForm.valid)
+    if(this.newEvent)
     {
-      const formValue = this.addEventForm.value;
-
-      const newEvent: EventDTO = {
-        uuid: '',
-        title: formValue.title,
-        description: formValue.description,
-        date: formValue.date,
-        location: formValue.location,
-        totalSeats: formValue.totalSeats,
-        availableSeats: formValue.totalSeats,
-        state: formValue.state,
-        eventCategory: formValue.eventCategory,
-        userUuid: formValue.userUuid
-      };
-
-      this.eventService.save(newEvent).subscribe({
+      this.newEvent.availableSeats = this.newEvent.totalSeats;
+      this.eventService.save(this.newEvent).subscribe({
         next: (res) => {
           this.events.push(res);
           this.addMode = false;
-          this.addEventForm.reset();
-          this.showValidationErrors = false;
         },
         error: (err) => {
           console.error('Errore creazione', err);
